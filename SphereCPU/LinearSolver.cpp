3960310326aa6010f1847d2813070f2b78ac67d4
LawrenceShum <lawrencesom1104@gmail.com>
2020-10-12 13:52:16 +0800

    update

--- SphereCPU/LinearSolver.cpp
+++ SphereCPU/LinearSolver.cpp
@@ -3,3 +3,19 @@
 
 using namespace std;
 using namespace Eigen;
+
+LinearSolver::LinearSolver(int row, int column)
+	:row(row),column(column)
+{
+
+}
+
+LinearSolver::~LinearSolver()
+{
+
+}
+
+void LinearSolver::fillA(double*)
+{
+	
+}
--- SphereCPU/LinearSolver.h
+++ SphereCPU/LinearSolver.h
@@ -6,9 +6,31 @@
 class LinearSolver
 {
 private:
+	//行
+	int row;
+	//列
+	int column;
+	//矩阵A，Ax=b
+	Eigen::MatrixXd	A;
+	//向量b
+	Eigen::VectorXd b;
 
+	//PCG解法
+	void PCG();
+	//Gauss-Seidel解法
+	void GS();
+	//jakobi解法
+	void Jakobi();
+	//将数据放入A矩阵中
+	void fillA(double*);
 
 public:
+	LinearSolver(int,int);
 
+	//获取A矩阵
+	Eigen::MatrixXd get_matrix_A();
+	//获取向量b
+	Eigen::VectorXd get_vector_b();
 
+	~LinearSolver();
 };
